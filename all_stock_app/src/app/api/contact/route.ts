c3ff27e996b54b72530aa2ef7f769af446af41eb
daito-kawasaki <daito.wb.b@gmail.com>
2024-07-26 10:30:50 +0900

    fix:メール送信機能の修正 secureの追加

--- all_stock_app/src/app/api/contact/route.ts
+++ all_stock_app/src/app/api/contact/route.ts
@@ -7,6 +7,7 @@ export async function POST(req:NextRequest) {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.GMAILUSER,
       pass: process.env.GMAILPASS,
