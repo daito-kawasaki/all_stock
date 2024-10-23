7bf42a05b341b0ea0242cca71c9adc69199ddaf1
daito-kawasaki <daito.wb.b@gmail.com>
2024-10-23 13:37:06 +0900

    scrollエラー発生のため仮コミット

--- all_stock_app/src/components/contact_sec.tsx
+++ all_stock_app/src/components/contact_sec.tsx
@@ -1,9 +1,12 @@
 import PartsContactForm from "./parts_contact_form";
 import Parts_ttl from "./parts_ttl";
-
+import { Element } from "react-scroll";
 export default function Contact_sec() {
   return (
-    <div className="contact_sec bg-[#cac1b9]/40 pt-[16.1vw] pb-[3.66vw]">
+    <div
+      id="contact"
+      className="contact_sec bg-[#cac1b9]/40 pt-[16.1vw] pb-[3.66vw]"
+    >
       <Parts_ttl ttl="お問い合わせ" sub_ttl="Contact" margin="mb-[7.32vw]" />
       <PartsContactForm />
     </div>
