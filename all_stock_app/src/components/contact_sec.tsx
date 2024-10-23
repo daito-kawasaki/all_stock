2ca8e2203aa72602813e612da7332e0f6d78a229
daito-kawasaki <daito.wb.b@gmail.com>
2024-10-23 13:37:06 +0900

    scrollエラー発生のため仮コミット

--- all_stock_app/src/components/contact_sec.tsx
+++ all_stock_app/src/components/contact_sec.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import PartsContactForm from "./parts_contact_form";
 import Parts_ttl from "./parts_ttl";
-
+import { Element } from "react-scroll";
 export default function Contact_sec() {
   const imgPath = "/img/";
   return (
@@ -15,8 +15,13 @@ export default function Contact_sec() {
           className="w-full h-auto"
         />
       </div>
-      <Parts_ttl ttl="お問い合わせ" sub_ttl="Contact" margin="mb-[7.32vw]" />
-      <PartsContactForm />
+      <div
+        id="contact"
+        className="contact_sec bg-[#cac1b9]/40 pt-[16.1vw] pb-[3.66vw]"
+      >
+        <Parts_ttl ttl="お問い合わせ" sub_ttl="Contact" margin="mb-[7.32vw]" />
+        <PartsContactForm />
+      </div>
     </div>
   );
 }
--- all_stock_app/src/components/header.tsx
+++ all_stock_app/src/components/header.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 import { zen_kaku_md } from "@/lib/fonts";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export default function Header() {
   const imgPath = "/img/";
@@ -20,8 +21,8 @@ export default function Header() {
           />
         </div>
         <div className="right flex items-center gap-10">
-          <Link
-            href=""
+          <AnchorLink
+            href="#contact"
             className={
               `${zen_kaku_md.className}` +
               " flex items-center gap-4 py-2 px-5 border border-zinc-800/70 rounded-3xl text-base text-black/70"
@@ -29,7 +30,7 @@ export default function Header() {
           >
             <FontAwesomeIcon icon={faPaperPlane} className=" w-4 h-4" />
             お問い合わせ
-          </Link>
+          </AnchorLink>
           <div className="w-[50px] h-[50px] rounded-[100px] border border-zinc-800 flex-col justify-center items-center gap-[7px] inline-flex">
             <div className="w-[30px] h-[0px] relative">
               <span className="w-[30px] h-[0px] left-0 top-0 absolute border border-zinc-800/70"></span>
